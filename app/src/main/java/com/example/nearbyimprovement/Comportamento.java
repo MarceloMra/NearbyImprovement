8eff78fb62dc0feff0b6af1cd8c935d8779c3088
MarceloMra <moreira.marcelo321@gmail.com>
2020-02-26 15:50:51 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/nearbyimprovement/Comportamento.java
@@ -0,0 +1,5 @@
+package com.example.nearbyimprovement;
+
+public enum Comportamento {
+    SUBSCRIBER, PUBLISHER, REQ_REPLY;
+}
---
+++ app/src/main/java/com/example/nearbyimprovement/NearbyAccessObject.java
@@ -0,0 +1,5 @@
+package com.example.nearbyimprovement;
+
+public class NearbyAccessObject {
+
+}
