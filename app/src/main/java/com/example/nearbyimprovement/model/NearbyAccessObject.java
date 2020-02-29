7b35243b18f0fc8670b491a40ac3522f3453cfba
MarceloMra <moreira.marcelo321@gmail.com>
2020-02-29 19:07:28 -0300

    Mudado o escopo de visibilidade dos métodos de serializar e desserializar para private.

--- app/src/main/java/com/example/nearbyimprovement/model/NearbyAccessObject.java
+++ app/src/main/java/com/example/nearbyimprovement/model/NearbyAccessObject.java
@@ -206,14 +206,14 @@ public class NearbyAccessObject {
         }
     }
 
-    public byte[] serialize(Object obj) throws IOException {
+    private byte[] serialize(Object obj) throws IOException {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         ObjectOutputStream os = new ObjectOutputStream(out);
         os.writeObject(obj);
         return out.toByteArray();
     }
 
-    public Object deserialize(byte[] data) throws IOException, ClassNotFoundException {
+    private Object deserialize(byte[] data) throws IOException, ClassNotFoundException {
         ByteArrayInputStream in = new ByteArrayInputStream(data);
         ObjectInputStream is = new ObjectInputStream(in);
         return is.readObject();
